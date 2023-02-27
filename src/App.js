54a2c57fd234afc3281f5e67b4473cb76d2b2732
Alyson Rehm <ALY.REHM@gmail.com>
2023-02-26 16:24:32 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [newItem, setNewItem] = useState ("");
+  const [toDos, setToDos] = useState ([]);
+
+  // always include with your forms 
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setToDos([...toDos, newItem]);
+    setNewItem("");
+  };
+
+ 
+  return(
+    <div>
+
+      {/* FORM  */}
+
+      <div>
+        <form onSubmit={(e) => {
+            handleSubmit(e);
+          }}>
+
+            <input type="text" value={newItem} 
+            onChange={(e) => {
+              setNewItem(e.target.value);
+            }}/>
+          <div>
+            <button>Add!</button>
+          </div>
+        </form>
+      </div>
+
+      {/* DISPLAY LIST  */}
+      <div>  
+        {toDos.map((todo, i) => {
+          return (
+            // adding the key ensures if the same item is added twice, it can be distingueshed. 
+            <div key={i}> 
+              <span>{todo}</span>
+              <button>Delete</button>
+            </div>
+          );  
+        })}
+      </div>
+    </div>  
   );
 }
 
